bed389f6c8810673638cbae64b7e21818b8d8383
msenador <smorian@yahoo.com>
2022-10-17 15:22:56 -0400

    error fix

--- src/app/services/data.service.ts
+++ src/app/services/data.service.ts
@@ -3,7 +3,7 @@ import { BehaviorSubject } from "rxjs";
 import { todoList } from "../fakeData/fakeData";
 import { Todo } from "../models/todo.model";
 
-@Injectable({providedIn: 'root'})
+@Injectable({providedIn: 'root'}) // without object ---> ERROR NullInjectorError: R3InjectorError(AppModule)
 export class DataService {
   private messageSource = new BehaviorSubject<string>("default message");
   private todoListOnLoad = new BehaviorSubject<Todo[]>([...todoList])
